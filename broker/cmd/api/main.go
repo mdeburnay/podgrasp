babe3e295824679ae5d001df0e8b23d9b0c685e5
Max De Burnay-Bastos <de_burnay@hotmail.co.uk>
2023-02-03 11:00:38 +0000

    refactored route handlers to gin gonic layout

--- broker/cmd/api/main.go
+++ broker/cmd/api/main.go
@@ -3,19 +3,13 @@ package main
 import (
 	"broker/cmd/routes"
 	"log"
-
-	"github.com/gin-contrib/cors"
 )
 
 const PORT = "80"
 
-type Config struct {}
-
 func main() {
 	log.Println("Starting Broker service on port: ", PORT)
-
 	r := routes.BrokerRouter()
-	r.Use(cors.Default())
 	r.Run(":" + PORT)
 	log.Println("Broker service started on port: ", PORT)
 }
--- broker/cmd/routes/routes.go
+++ broker/cmd/routes/routes.go
@@ -2,16 +2,23 @@ package routes
 
 import (
 	"net/http"
+	"os"
 
-	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func BrokerRouter() *gin.Engine {
+	env := os.Getenv("ENV")
+
+	if env == "development" {
+		err := godotenv.Load()
+		if err != nil {
+			panic("Error loading .env file")
+		}
+	}
+
 	r := gin.Default()
-
-	r.Use(cors.Default())
-
 	r.GET("/", func(c *gin.Context) {
 		c.String(http.StatusOK, "Hello from Broker!")
 	})
