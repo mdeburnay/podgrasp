2d524c571cf4fe6548b82774ca2afdfd4925c07b
Max De Burnay-Bastos <de_burnay@hotmail.co.uk>
2023-05-20 20:00:32 +0100

    added mongodb connection logic

--- logger/cmd/main.go
+++ logger/cmd/main.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"log"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
 const (
 	webPort  = "80"
 	rpcPort  = "5001"
@@ -7,4 +14,31 @@ const (
 	gRpcPort = "50001"
 )
 
-func main() {}
+var client *mongo.Client
+
+type Config struct {
+}
+
+func main() {
+	mongoClient, err := connectToMongo()
+	if err != nil {
+		log.Panic(err)
+	}
+
+	client = mongoClient
+}
+
+func connectToMongo() (*mongo.Client, error) {
+	clientOptions := options.Client().ApplyURI(mongoURL)
+	clientOptions.SetAuth(options.Credential{
+		Username: "admin",
+		Password: "password",
+	})
+
+	client, err := mongo.Connect(nil, clientOptions)
+	if err != nil {
+		return nil, err
+	}
+
+	return client, nil
+}
